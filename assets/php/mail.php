a2bf85460e4943f5f6be6454bd5134b861619910
Kate Engard <kateengard@Kates-MacBook-Pro.local>
2021-07-08 16:19:35 -0700

    update mail php

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -18,7 +18,7 @@ header("Access-Control-Allow-Origin: *");
         }
 
         // Set the recipient email address.
-        $recipient = "you@email.here";
+        $recipient = "kateengard@gmail.com";
 
         // Set the email subject.
         $subject = "HairSalon - Mail From $name";
